fd2467860a0f6680be5606c2a3086194f4a491de
kashif-ali-sqa <raokashif.bwn47@gmail.com>
2024-01-23 15:10:44 +0500

    pagefactory

--- src/test/java/objectpack/demoLoginLocators.java
+++ src/test/java/objectpack/demoLoginLocators.java
@@ -3,29 +3,39 @@ package objectpack;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class demoLoginLocators {
 	WebDriver driver;
+
 	public demoLoginLocators(WebDriver driver) {
-		
+
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
-	
-	private By emailField = By.id("input-email");
-	private By passwordField = By.id("input-password");
-	private By btnLogin  = By.cssSelector("input[value='Login']");
-	
+
+	@FindBy(id = "input-email")
+	private WebElement emailField;
+
+	@FindBy(id = "input-password")
+	private WebElement passwordField;
+	@FindBy(css = "input[value='Login']")
+	private WebElement btnLogin;
+
 	public WebElement emailField() {
-		
-		return driver.findElement(emailField);
+
+		return emailField;
 	}
-public WebElement passwordField() {
-		
-		return driver.findElement(passwordField);
+
+	public WebElement passwordField() {
+
+		return passwordField;
 	}
-public WebElement btnLogin() {
-	
-	return driver.findElement(btnLogin);
-}
+
+	public WebElement btnLogin() {
+
+		return btnLogin;
+	}
 
 }
